45a760a206d9a04bbb209c4cd8fc19fcd7f06b5c
YigitYeler <yeleryigit40@gmail.com>
2023-03-25 16:26:43 +0300

    Function :: Password encrypted

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,17 @@
 import User from "../models/User.js";
 import mongoose from "mongoose";
-
+import sha1 from 'sha1'
 export const register = async (req, res) => {
-    const user = new User(req.body)
-    console.log(req.body)
+    const user = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: sha1(req.body.password),
+    })
     try {
         await user.save()
+        res.status(200).json({
+            message: "User registered succesfully"
+        })
     } catch (error) {
         res.status(409).json({
             message: error.message
