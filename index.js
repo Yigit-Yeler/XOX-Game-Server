2484d91bffeba73ccb4239d5405593c3f1a4de9d
YigitYeler <yeleryigit40@gmail.com>
2023-04-10 23:56:41 +0300

    Added socket io

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import authRoutes from './routes/auth.js'
+import http from 'http'
+import { Server } from 'socket.io';
+
 const app = express();
+const server = http.createServer(app)
+const socket = new Server(server)
 dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
@@ -20,6 +25,7 @@ app.get('/', (req, res) => {
 
 app.use("/auth", authRoutes)
 
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
@@ -29,7 +35,9 @@ mongoose
         dbName: 'xox'
     })
     .then(() => {
-        app.listen(PORT, () => {
+
+
+        server.listen(PORT, () => {
             console.log(`Server is running on port: ${PORT}`)
         })
 
@@ -37,3 +45,15 @@ mongoose
     .catch((e) => {
         console.log(e)
     })
+socket.on('connection', (socket) => {
+    socket.on('joinGame', () => {
+        console.log('Odaya katılındı')
+        socket.join('room1')
+    });
+
+    socket.on('inRoom', () => {
+        console.log('Odaya gönerildi')
+        socket.to('room1').emit('haber', socket.id)
+    });
+});
+
